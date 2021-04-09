ef96c9279b49d554b83710546c4823b217b27ffc
duyettwyler2k <duyettwyller@gmail.com>
2021-04-09 15:08:42 +0700

    'asdsa'

---
+++ src/Point2D.java
@@ -0,0 +1,47 @@
+public class Point2D {
+    public static final float[] XY = new float[2];
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    Point2D() {
+    }
+
+    Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public float[] getXY() {
+        return new float[]{this.x, this.y};
+    }
+
+    @Override
+    public String toString() {
+        return "Point2D{" +
+                "x=" + this.x +
+                ", y=" + this.y +
+                '}';
+    }
+}
